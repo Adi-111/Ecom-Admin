63e8925a14ee02912f51de1c4334905af75c552c
Adi-111 <Chandradityasingh11@gmail.com>
2024-08-08 08:06:04 +0530

    fixed some import errors
    Signed-off-by: Adi-111 <Chandradityasingh11@gmail.com>

--- app/(dashboard)/[storeId]/(routes)/colors/[colorId]/_components/color-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/colors/[colorId]/_components/color-form.tsx
@@ -13,7 +13,7 @@ import { Size } from "@prisma/client";
 import { Trash } from "lucide-react";
 
 import { Heading } from "@/components/heading";
-import AlertModal from "@/components/modals/alert-modal";
+import AlertModal from "@/components/models/alert-modal";
 import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
